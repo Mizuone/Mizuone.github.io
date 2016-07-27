2fab59ef960c38640f3a19b5caebefde40f1955d
Kyle Orlinski <orlinskikyle@gmail.com>
2016-07-27 15:46:07 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+"use strict";
+$(document).ready(function() {
+    console.log("connected");
+    var PortfolioSpace = {};
+    PortfolioSpace = function() {
+        $("#testprogress").progressbar({
+           value: 50 
+        });
+        //$()
+    };
+    PortfolioSpace();
+});
