6458d6e20c26f1d0de4f8f8899b9da2b37ee0614
Kyle Orlinski <orlinskikyle@gmail.com>
2016-07-28 16:23:34 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,14 +7,24 @@ $(document).ready(function() {
             console.log($(this).scrollTop());
            if($(this).scrollTop() > 80) {
                $("ul.progress-stlye li").css("display", "inline");
-               $("ul.progress-style li").fadeIn(400,function(){});
+               $("ul.progress-style li").fadeIn(1000,function(){});
            } else {
                $("ul.progress-style li").fadeOut("fast",function(){});
            }
-           if($(this).scrollTop() > 500) {
-               $(".projectdivider").fadeIn("slow",function(){});
+           if($(this).scrollTop() > 362) {
+               $(".firstrow").fadeIn(500,function(){});
            } else {
-               $(".projectdivider").fadeOut("fast",function(){});
+               $(".firstrow").fadeOut("fast",function(){});
+           }
+           if($(this).scrollTop() > 762) {
+               $(".secondrow").fadeIn(500,function(){});
+           } else {
+               $(".secondrow").fadeOut("fast",function(){});
+           }
+           if($(this).scrollTop() > 1162) {
+               $(".thirdrow").fadeIn(500,function(){});
+           } else {
+               $(".thirdrow").fadeOut("fast",function(){});
            }
         });
         function fadeobjectin(object) {
