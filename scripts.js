5c07c7c89eff482b92a33f0dea719a0d21515b61
Kyle Orlinski <orlinskikyle@gmail.com>
2016-07-28 15:56:03 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,10 +3,26 @@ $(document).ready(function() {
     console.log("connected");
     var PortfolioSpace = {};
     PortfolioSpace = function() {
-        $("#testprogress").progressbar({
-           value: 50 
+        $(window).scroll(function() {
+            console.log($(this).scrollTop());
+           if($(this).scrollTop() > 80) {
+               $("ul.progress-stlye li").css("display", "inline");
+               $("ul.progress-style li").fadeIn(400,function(){});
+           } else {
+               $("ul.progress-style li").fadeOut("fast",function(){});
+           }
+           if($(this).scrollTop() > 500) {
+               $(".projectdivider").fadeIn("slow",function(){});
+           } else {
+               $(".projectdivider").fadeOut("fast",function(){});
+           }
         });
-        //$()
+        function fadeobjectin(object) {
+            return $(object).fadeIn("slow",function(){});
+        }
+        function fadeobjectOut(object) {
+            return $(object).fadeOut("slow",function(){});
+        }
     };
     PortfolioSpace();
 });
