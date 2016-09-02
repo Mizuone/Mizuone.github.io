de76b9175ca910462c4ee91187cffc9b68d40d1e
Kyle Orlinski <orlinskikyle@gmail.com>
2016-09-02 11:29:32 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -21,17 +21,17 @@ $(document).ready(function() {
                 
                 $(".html5bar .novice").animate({ width: "20%" }, 25);
                 $(".html5bar .journeyman").animate({ width: "25%" }, 300);
-                $(".html5bar .epic").animate({ width: "20%" }, 500);
-                $(".html5bar .legendary").animate({ width: "15%" }, 700);
+                $(".html5bar .epic").animate({ width: "15%" }, 500);
+                $(".html5bar .legendary").animate({ width: "0%" }, 700);
                 
                 $(".jquerybar .novice").animate({ width: "20%" }, 25);
                 $(".jquerybar .journeyman").animate({ width: "25%" }, 300);
                 $(".jquerybar .epic").animate({ width: "5%" }, 500);
                                 
-                $(".phpbar .novice").animate({ width: "20%" }, 25);
-                $(".phpbar .journeyman").animate({ width: "10%" }, 300);
+                $(".phpbar .novice").animate({ width: "15%" }, 25);
+                $(".phpbar .journeyman").animate({ width: "0%" }, 300);
                                 
-                $(".nodejsbar .novice").animate({ width: "15%" }, 300);
+                $(".nodejsbar .novice").animate({ width: "20%" }, 300);
                 
                 isAnimated++;
             }
