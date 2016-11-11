47c5da283b5a2c556d3af21d783231d937619d2f
Kyle Orlinski <orlinskikyle@gmail.com>
2016-11-11 10:34:34 -0600

    Add files via upload

---
+++ projects/minimu/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
+
+
+gulp.task('sass',function() {
+   gulp.src('scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+gulp.task('serve', function() {
+   browserSync.init({
+       server: {
+           baseDir: "./"
+       }
+   });
+    
+    
+    gulp.watch('./scss/*.scss', ['sass']);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['sass', 'serve']);
