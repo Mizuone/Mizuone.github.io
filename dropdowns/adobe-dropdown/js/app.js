9350491e0fe9f319f47cb185b4342d3b505d9c5a
Kyle Orlinski <orlinskikyle@gmail.com>
2016-11-15 23:09:36 -0600

    Add files via upload

---
+++ dropdowns/adobe-dropdown/js/app.js
@@ -0,0 +1,15 @@
+/*"use strict";
+
+    $(document).ready(function() {
+       //when user hovers over nav transition opacity
+       
+       (function() {
+           $('.nav-menu').on("mouseenter", function() {
+              $(this).find(".sub-nav").css({'opacity':0}).animate({'opacity':1},300);
+            });
+           $('.sub-nav').on("mouseleave", function() {
+               console.log("Hlea");
+               $(this).css({'opacity':1}).animate({'opacity':0},300);
+            });
+       })();
+    });*/
