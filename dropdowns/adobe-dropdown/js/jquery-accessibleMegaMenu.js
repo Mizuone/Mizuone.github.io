fe2e39cd83da59578606dbc6011665a97be06aab
Kyle Orlinski <orlinskikyle@gmail.com>
2016-12-05 16:41:33 -0600

    Add files via upload

--- dropdowns/adobe-dropdown/js/jquery-accessibleMegaMenu.js
+++ dropdowns/adobe-dropdown/js/jquery-accessibleMegaMenu.js
@@ -286,7 +286,7 @@ limitations under the License.
                 panel = target.closest('.' + this.settings.panelClass);
              if (isTouch) {
                         event.preventDefault();
-                        //event.stopPropagation();
+                        event.stopPropagation();
                         _togglePanel.call(this, event, target.hasClass(this.settings.openClass));
                     }
             if (topli.length === 1
