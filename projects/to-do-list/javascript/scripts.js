5ba1b67a6d10cd2de9a7673988edcecc1ea5d9ae
Kyle Orlinski <orlinskikyle@gmail.com>
2017-06-10 15:23:19 -0500

    Add files via upload

---
+++ projects/to-do-list/javascript/scripts.js
@@ -0,0 +1,71 @@
+if (typeof(listSpace === "undefined")) {
+    var listSpace = {};
+}
+(function() {
+    if (localStorage['tasks']) {
+        var tasks = JSON.parse(localStorage['tasks']);
+    }else {
+        var tasks = [];
+    }
+    $(document).ready(function() {
+        //adds input item to container
+        listSpace = function() {
+            var i = 0;
+
+            function createItem(id, value) {
+                $(".createditems").append("<p class='createditem' id='" + id + "'>" + "<span>" + value + "</span>"  + "&nbsp&nbsp&nbsp&nbsp&nbsp" +
+                                        "<a href='#'><span class='glyphicon glyphicon-remove deleteitem'></span></a>" +
+                                        "</p>");
+            }
+
+            $("#inputcreateitem").keyup(function(e) {
+                var key = e.which;
+                if (key === 13 && $("#inputcreateitem").val() !== " ") {
+                    var getValue = $("#inputcreateitem").val();
+                    var taskID = "task-" + i;
+                    createItem(taskID, getValue);
+
+                    listSpace.addDragAndDrop();
+                    $("#inputcreateitem").val("");
+                    tasks.push(getValue);
+                    i++;
+                    localStorage.setItem(taskID, getValue);
+                }
+            });
+            listSpace.addDragAndDrop = function() {
+             $(".container").droppable({
+                            accept: "p.createditem",
+                            tolerance: "pointer",
+                            drop: function(event, ui) {
+                                $(this).addClass("cell-dropped");
+                                $(ui.draggable).appendTo(this);
+                            }
+                        });
+                        $("p.createditem").draggable({
+                            opacity: 0.7,
+                            helper: 'clone',
+                            scroll: true
+                        });
+                    $(".createditems ,.container").on('mouseover', 'p', function (e) {
+                        self = $(this);
+                        taskID = self.attr('id');
+                        $(".createditems , .container").on('click', 'a', function (e) {
+                            $("#" + taskID).remove();
+                            localStorage.removeItem(taskID);
+                        });
+                    });
+            }
+            for(var i = 0; i < localStorage.length; i++) {
+                var taskID = localStorage.key(i);
+
+                if (taskID.indexOf('task') !== -1) {
+                  createItem(taskID, localStorage.getItem(taskID));
+                  listSpace.addDragAndDrop();
+                }
+
+            }
+        }
+        listSpace();
+    });
+    //window.localStorage.clear();
+})();
---
+++ projects/wikiviewer/wikiviewer.js
@@ -0,0 +1,75 @@
+"use strict";
+$(document).ready(function() {
+   var WikiSpace = {};
+    WikiSpace = function() {
+        $("#formcontrol input").keypress(function (e){
+           if (e.which == 13) {
+               var formInput = $(this).val(),
+                   wikiapi = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + formInput + "&format=json&callback=?",
+                   wikiref = "https://en.wikipedia.org/wiki/";
+
+               wikiResult(wikiapi, wikiref, formInput);
+               e.preventDefault();
+           }
+        });
+        function wikiResult(wikiEntry, wikiSite, titleContent) {
+               $.ajax({
+                   url: wikiEntry,
+                   type: 'GET',
+                   contentType: "application/json; charset=utf=8",
+                   async: true,
+                   dataType: 'json',
+                   success: function(data, status, jqXHR){
+                        $(".responsecontainer").fadeOut(0 ,function(){
+                            $(".responsecontainer").html("<h1>You Searched ''" + data[0] + "'' ");
+                            $(".responsecontainer h1").addClass("headingresult");
+                            $(".responsecontainer").append('<ul class="resultformat">' + "</ul>");
+                            for (var i = 0; i < data[1].length; i++) {
+                                $(".responsecontainer ul").append('<a href="'+ wikiSite + data[1][i] + '" target="_blank">' +
+                   "<li>" +
+                        '<div class="row">' +
+                            '<div class="col-lg-4">' +
+                                "<h2>" + data[1][i] + "</h2>" +
+                            "</div>"
+                        +   '<div class="col-lg-8">' +
+                                "<p>" + data[2][i] + "</p>" +
+                            "</div>" +
+                        "</div>"
+                   + "</li></a><br>");
+                            }
+                        });
+                        $(".responsecontainer").fadeIn();
+                        var styleListItem = document.querySelectorAll(".resultformat li");
+                        for (var x = 0; x < styleListItem.length; x++) {
+                            $(styleListItem[x]).on("mouseover", function() {
+                                $(this).css({
+                                  "transition": "0.1s ease-in-out",
+                                  "border-left": "solid 3px red"
+                                });
+                            })
+                            $(styleListItem[x]).on("mouseleave", function() {
+                                $(this).css({
+                                  "transition": "0.1s ease-in-out",
+                                  "border-left": "none"
+                                });
+                            })
+                        }
+                   },
+               })
+               .done(function() {
+                   console.log("success");
+               })
+               .fail(function() {
+                   console.log("Failed");
+               })
+               .always(function() {
+                 console.log("Complete");
+               });
+               $("#formcontrol input").val('');
+        }
+        $("#randombutton").on("click", function() {
+                window.location.href = "https://en.wikipedia.org/wiki/Special:Random";
+        });
+    };
+    WikiSpace();
+});
