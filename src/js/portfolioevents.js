44fca6523a505a27e8500fda6ad84b03e3dbe997
Kyle Orlinski <orlinskikyle@gmail.com>
2023-10-12 16:34:50 -0500

    cleanup and bug fixing

--- src/js/portfolioevents.js
+++ src/js/portfolioevents.js
@@ -1,13 +1,20 @@
 export class PortfolioEvents {
     static topContainerEvent() {
-        window.addEventListener('scroll', function () {
-            if (pageYOffset > 200) {
-                document.querySelector('.backtotop').style.opacity = '1';
+        let backToTop = document.querySelector('.backtotop');
+        
+        function interactableElement(element) {
+            if (scrollY > 200) {
+                element.classList.remove('not-clickable');
             } else {
-                document.querySelector('.backtotop').style.opacity = '0';
+                element.classList.add('not-clickable');
             }
+        }
 
+        window.addEventListener('scroll', function () {            
+            interactableElement(backToTop);
         });
+
+        interactableElement(backToTop);
     };
 
     static hamburgerMenuClickEvents() {
